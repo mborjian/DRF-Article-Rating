288ebbcd90202390102486e32089eee0d23355bd
Mehdi <mim.borjian@gmail.com>
2024-04-18 08:49:58 +0330

    Models & Migration
    models: Article, Rating
    ** migration is necessary

--- article/models.py
+++ article/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(6)])
+
+    class Meta:
+        unique_together = ('article', 'user')
+
+    def __str__(self):
+        return f"{self.user.username}'s rating for {self.article.title}"
