d5485e3817bbfa2ae78a786473d7a31cbbd8033d
Mehdi <mim.borjian@gmail.com>
2024-04-18 09:08:19 +0330

    update views
    added ArticleListView & RatingCreateView

--- article/views.py
+++ article/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .models import Article, Rating
+from .serializers import RatingSerializer, ArticleListSerializer
 
 
 @login_required
@@ -8,3 +13,18 @@ def home(request):
     context = {'articles': articles}
     return render(request, 'article/home.html', context)
 
+
+class ArticleListView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleListSerializer
+
+
+class RatingCreateView(generics.CreateAPIView):
+    serializer_class = RatingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Rating.objects.filter(user=self.request.user)
