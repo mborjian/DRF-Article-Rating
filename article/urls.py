dadbfc866583093109b7324bc96166b5b2ab6968
Mehdi <mim.borjian@gmail.com>
2024-04-18 09:07:43 +0330

    create a simple landing

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from article.views import home
 
 urlpatterns = [
-    path('home/', home, name='article.home'),
+    path('', home, name='article.home'),
 ]
--- article/views.py
+++ article/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 @login_required
 def home(request):
-    return render(request, 'article/home.html')
+    articles = Article.objects.order_by('-id')[:5]
+    context = {'articles': articles}
+    return render(request, 'article/home.html', context)
+
