eb4e2ba8d38caeb8f4b6abc8f7775a042c4f5fb3
Mehdi <mim.borjian@gmail.com>
2024-04-18 10:31:06 +0330

    add needed urls 4 rating

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from article.views import home
+from article.views import home, rate_article_view, article_detail_view
 
 urlpatterns = [
     path('', home, name='article.home'),
+    path('article/<int:article_id>/', article_detail_view, name='article_detail'),
+    path('article/<int:article_id>/rate/', rate_article_view, name='rate_article'),
+    path('article/<int:article_id>/update/', rate_article_view, name='update_rating'),
 ]
