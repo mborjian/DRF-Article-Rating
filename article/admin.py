da4c38f64228cad118180e4bacb91a996c0f7c15
Mehdi <mim.borjian@gmail.com>
2024-04-18 10:30:48 +0330

    register models in admin

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article, Rating
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class RatingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Rating, RatingAdmin)
