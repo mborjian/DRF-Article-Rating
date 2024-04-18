9dd71d595568fc0f80bfde844e26271ae0df8a11
Mehdi <mim.borjian@gmail.com>
2024-04-18 10:31:25 +0330

    update views for rating articles

--- article/views.py
+++ article/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.db.models import Count, Avg
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
@@ -7,11 +8,45 @@ from .models import Article, Rating
 from .serializers import RatingSerializer, ArticleListSerializer
 
 
-@login_required
 def home(request):
-    articles = Article.objects.order_by('-id')[:5]
+    articles = Article.objects.annotate(
+        rating_count=Count('rating', distinct=True),
+        average_rating=Avg('rating__rating')
+    ).order_by('id')
+
+    if request.user.is_authenticated:
+        user_ratings = {rating.article_id: rating for rating in Rating.objects.filter(user=request.user)}
+        for article in articles:
+            article.user_rating = user_ratings.get(article.id)
+
     context = {'articles': articles}
     return render(request, 'article/home.html', context)
+
+
+@login_required
+def rate_article_view(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+
+    if request.method == 'POST':
+        rating_value = int(request.POST.get('rating', 0))
+        if 0 <= rating_value <= 5:
+            rating, created = Rating.objects.get_or_create(user=request.user, article=article)
+            rating.rating = rating_value
+            rating.save()
+
+    return redirect('article_detail', article_id=article_id)
+
+
+def article_detail_view(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    user_rating = None
+    if request.user.is_authenticated:
+        user_rating = Rating.objects.filter(user=request.user, article=article).first()
+
+    average_rating = Rating.objects.filter(article=article).aggregate(Avg('rating'))['rating__avg']
+
+    context = {'article': article, 'user_rating': user_rating, 'average_rating': average_rating}
+    return render(request, 'article/article_detail.html', context)
 
 
 class ArticleListView(generics.ListAPIView):
